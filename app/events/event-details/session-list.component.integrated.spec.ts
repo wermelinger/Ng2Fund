603d234bc9ac8d66b013972d5262483ba5bde293
Stefan Wermelinger <stefan.wermelinger@abix.ch>
2017-08-30 20:24:35 +0200

    Finished integrated test.

--- app/events/event-details/session-list.component.integrated.spec.ts
+++ app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing'
-import { DebugElement } from '@angular/core'
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core'
 import { SessionListComponent } from './session-list.component'
+import { UpvoteComponent } from './upvote.component'
+import { DurationPipe } from '../shared/duration.pipe'
+import { CollapsibleWellComponent } from '../../common/collapsible-well.component'
 import { AuthService } from '../../user/auth.service'
 import { VoterService } from './voter.service'
 import { ISession } from '../shared/event.model'
@@ -13,14 +16,14 @@ describe('SessionListComponent', () => {
         debugEl: DebugElement
 
     beforeEach(async(() => {
-        let mockAuthService = {}
-        let mockVoterService = {}
+        let mockAuthService = { isAuthenticated: () => true, currentUser: { userName: 'Joe'} }
+        let mockVoterService = { userHasVoted: () => true }
 
         TestBed.configureTestingModule({
             imports: [],
-            declarations: [SessionListComponent],
+            declarations: [SessionListComponent, /*UpvoteComponent, */ DurationPipe,/* CollapsibleWellComponent*/],
             providers: [{ provide: AuthService, useValue: mockAuthService }, { provide: VoterService, useValue: mockVoterService }],
-            schemas: []
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents()
     }))
 
@@ -29,5 +32,23 @@ describe('SessionListComponent', () => {
         component = fixture.componentInstance
         debugEl = fixture.debugElement
         element = fixture.nativeElement
+    })
+
+    describe('initial display', () => {
+        it('should have the correct session title', () => {
+            // Arrange
+            component.sessions = [{id: 3, name: 'Session 1', presenter: 'Joe', duration: 1, level: 'beginner', abstract: 'abstract', voters: ['john', 'bob']}]
+            component.filterBy = 'all'
+            component.sortBy = 'name'
+            component.eventId = 4
+
+            // Act
+            component.ngOnChanges()
+            fixture.detectChanges()
+
+            // Assert
+            expect(element.querySelector('[well-title]').textContent).toContain('Session 1')
+            expect(debugEl.query(By.css('[well-title]')).nativeElement.textContent).toContain('Session 1')
+        })
     })
 })
